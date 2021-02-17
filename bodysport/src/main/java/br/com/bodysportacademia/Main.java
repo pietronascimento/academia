f6cce366c090b9433ca08fb3ef32203c60a45752
Pietro do Nascimento <pietronascimento@gmail.com>
2021-02-16 21:07:52 -0300

    Add files via upload

--- bodysport/src/main/java/br/com/bodysportacademia/Main.java
+++ bodysport/src/main/java/br/com/bodysportacademia/Main.java
@@ -1,8 +1,10 @@
 /**
  * @(#) Main.java 1.0 05/01/2021
  *
- * Copyright 2014 P Softwares, Todos os direitos reservados. PROPRIEDADE P
- * Softwares/CONFINDECIAL. Uso sujeito a condicoes de licenca.
+  * Copyright 2014 Pietro do Nascimento, Todos os direitos reservados. 
+ * PROPRIEDADE Pietro do Nascimento/CONFINDECIAL. 
+ * Uso sujeito a condicoes de licenca.
+ * 
  */
 package br.com.bodysportacademia;
 
@@ -14,8 +16,10 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 import br.com.bodysportacademia.view.ViewInit;
+import br.com.bodysportacademia.view.ViewLogin;
 
 /**
  * @version 1.0 05/01/2021
@@ -32,13 +36,32 @@ public class Main extends UI {
 		@Override
 		protected void servletInitialized() throws ServletException {
 			super.servletInitialized();
+			getService().addSessionInitListener(new ValoThemeSessionInitListener());
 		}
 	}
 	
 	@Override
 	protected void init(VaadinRequest request) {
 		super.getPage().setTitle("BodySport v0.2");
-		super.setSizeFull();				
-		super.setContent(new ViewInit());
+		super.setSizeFull();
+		
+		v_layout = new VerticalLayout();
+		v_layout.setMargin(true);
+		v_layout.setSizeFull();
+		super.setContent(v_layout);
+		
+		viewLogin();
 	}
+	
+	public void viewLogin() {
+		v_layout.removeAllComponents();
+		v_layout.addComponent(new ViewLogin(Main.this));
+	}
+	
+	public void viewInit() {
+		v_layout.removeAllComponents();
+		v_layout.addComponent(new ViewInit(Main.this));
+	}
+	
+	private VerticalLayout v_layout;
 }
